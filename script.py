63a25bd3519a3fca8d42eae89f8dc1cd97d91330
Murilo Martin <murilodsmartin@gmail.com>
2023-04-12 22:21:39 -0300

    Add log to track best path

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from functions import *
 
 
-fileData = readFile('testeAlgo.txt')
+fileData = readFile('caverna_dragao_v2.txt')
 weightMatrix = fileData[0]
 eventsDict = fileData[1]
 adjMatrix = createGraph(weightMatrix)
@@ -11,11 +11,16 @@ heuristic = createDistanceMatrix(weightMatrix)
 # print('weightMatrix : ',weightMatrix)
 print('adjMatrix : ',adjMatrix)
 
-for i in range (2,3):
+for i in range (2,28):
     if i == 11 or i == 12 or i==28: #Faltou o "C" e o "Z" no arquivo do mapa
         break
     goal = eventsDict[i]
     start = eventsDict[i-1]
+    print('\n\n\ngoal',goal['node'])
+    print('start',start['node'])
     print('Calculando entre : %s e %s\n' % (start['label'], goal['label']))
-    shortest = a_star(adjMatrix,heuristic,start['node'],goal['node'])[0]
+    results = a_star(adjMatrix,heuristic,start['node'],goal['node'])
+    path = results[1]
+    shortest = results[0]
     print('Caminho menos custoso entre : %s e %s : %d \n' % (start['label'], goal['label'], shortest))
+    print('Caminho : ',path)
